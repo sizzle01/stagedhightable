2038657349961af2828e45849c4d3b27ed73692f
Your Name <you@example.com>
2022-03-24 11:49:21 +0100

    i added place holder map

--- src/components/address.tsx
+++ src/components/address.tsx
@@ -1,5 +1,6 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { GoogleMap, Marker } from 'react-google-maps'
 
 const addressInformation = [
   {
@@ -51,6 +52,16 @@ const Address = () => {
             Stop wasting time and money designing and managing a website. that
             doesn’t get results. Happiness guaranteed!
           </Text>
+        </Box>
+        <Box mb="30px" w={['90%', '90%', '80%', '70%']}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3325395304414!2d-122.01116148467422!3d37.33463524513264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb59127ce078f%3A0x18e1c3ce7becf1b!2sApple%20Park!5e0!3m2!1sen!2sin!4v1637309850935!5m2!1sen!2sin"
+            width="100%"
+            height="400px"
+            style={{ border: '0' }}
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
         </Box>
         <Box
           display="flex"
